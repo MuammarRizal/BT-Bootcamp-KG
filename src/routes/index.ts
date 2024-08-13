b7a50353471373d5cb68c13e1b94ff490b275b94
MuammarRizal <muammarrizal31@gmail.com>
2024-08-13 14:55:40 +0700

    add method API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,15 @@
 import { Router, Request, Response } from "express";
 
 const router = Router();
-router.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
-
 interface User {
+  id: number;
   full_name: string;
   email: string;
 }
 
 const users: User[] = [];
 
-router.get("/user", (req: Request, res: Response) => {
+router.get("/users", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: "Berhasil mendapatkan data user",
@@ -23,9 +20,9 @@ router.get("/user", (req: Request, res: Response) => {
 });
 
 router.post("/users", (req: Request, res: Response) => {
-  const user: User = req.body;
+  const inputUser: User = req.body;
 
-  const index = users.findIndex((user) => user.email === user.email);
+  const index = users.findIndex((user) => user.email === inputUser.email);
 
   if (index !== -1) {
     res.status(400).json({
@@ -33,17 +30,72 @@ router.post("/users", (req: Request, res: Response) => {
       message: "email sudah terdaftar",
       data: {},
     });
+    return;
   }
 
   users.push({
-    full_name: user.full_name,
-    email: user.email.toLocaleLowerCase(),
+    id: +new Date(),
+    full_name: inputUser.full_name,
+    email: inputUser.email.toLocaleLowerCase(),
   });
 
   res.status(201).json({
     success: true,
-    message: "User berhasil ditambahkan",
-    data: user,
+    message: "user berhasil ditambahkan",
+    data: inputUser,
   });
 });
+
+router.put("/users/:email", (req: Request, res: Response) => {
+  const email = req.params.email.toLocaleLowerCase();
+  const user: User = req.body;
+
+  const index = users.findIndex((user) => user.email === email);
+
+  if (index === -1) {
+    res.status(404).json({
+      success: false,
+      message: "User tidak ditemukan",
+      data: {},
+    });
+    return;
+  }
+
+  users[index] = {
+    id: +new Date(),
+    full_name: user.full_name,
+    email: user.email.toLowerCase(),
+  };
+
+  res.status(200).json({
+    success: true,
+    meesage: "User berhasil perbarui",
+    data: {
+      user,
+    },
+  });
+});
+
+router.delete("/user/:email", (req: Request, res: Response) => {
+  const email = req.params.email.toLocaleLowerCase();
+
+  const index = users.findIndex((item) => item.email === email);
+
+  if (index === -1) {
+    res.status(404).json({
+      message: "User tidak ditemukan",
+      success: false,
+      data: {},
+    });
+    return;
+  }
+  users.splice(index, 1);
+
+  res.status(200).json({
+    message: "Data berhasil dihapus",
+    success: true,
+    data: {},
+  });
+});
+
 export default router;
